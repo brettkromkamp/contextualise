5733822e22f186d19465e48fcba7e602391e212e
Brett Kromkamp <brett.kromkamp@gmail.com>
2019-05-18 12:44:38 +0200

    Continuation of (separate) notes-related functionality (un-tested).

--- contextualise/note.py
+++ contextualise/note.py
@@ -1,6 +1,14 @@
-from flask import (Blueprint, render_template)
-from flask_security import login_required
+from datetime import datetime
+
+import maya
+import mistune
+from flask import (Blueprint, flash, render_template, request, url_for, redirect)
+from flask_security import login_required, current_user
+from topicdb.core.models.attribute import Attribute
+from topicdb.core.models.datatype import DataType
+from topicdb.core.models.occurrence import Occurrence
 from topicdb.core.store.retrievaloption import RetrievalOption
+from werkzeug.exceptions import abort
 
 from contextualise.topic_store import get_topic_store
 
@@ -14,9 +22,15 @@ def index(map_identifier):
 
     topic = topic_store.get_topic(map_identifier, 'home')
 
-    notes = topic_store.get_topic_occurrences(map_identifier, 'notes', 'note',
-                                              inline_resource_data=RetrievalOption.INLINE_RESOURCE_DATA,
-                                              resolve_attributes=RetrievalOption.RESOLVE_ATTRIBUTES)
+    note_occurrences = topic_store.get_topic_occurrences(map_identifier, 'notes', 'note',
+                                                         inline_resource_data=RetrievalOption.INLINE_RESOURCE_DATA,
+                                                         resolve_attributes=RetrievalOption.RESOLVE_ATTRIBUTES)
+    notes = []
+    for note_occurrence in note_occurrences:
+        notes.append({'identifier': note_occurrence.identifier,
+                      'title': note_occurrence.get_attribute_by_name('title').value,
+                      'timestamp': maya.parse(note_occurrence.get_attribute_by_name('modification-timestamp').value),
+                      'text': mistune.markdown(note_occurrence.resource_data.decode())})
 
     return render_template('note/index.html',
                            topic_map=topic_map,
@@ -30,5 +44,64 @@ def add(map_identifier):
     topic_store = get_topic_store()
     topic_map = topic_store.get_topic_map(map_identifier)
 
+    if current_user.id != topic_map.user_identifier:
+        abort(403)
+
+    topic = topic_store.get_topic(map_identifier, 'home',
+                                  resolve_attributes=RetrievalOption.RESOLVE_ATTRIBUTES)
+    if topic is None:
+        abort(404)
+
+    form_note_title = ''
+    form_note_text = ''
+    form_note_scope = '*'
+
+    error = 0
+
+    if request.method == 'POST':
+        form_note_title = request.form['note-title'].strip()
+        form_note_text = request.form['note-text']
+        form_note_scope = request.form['note-scope'].strip()
+
+        # If no values have been provided set their default values
+        if not form_note_scope:
+            form_note_scope = '*'  # Universal scope
+
+        # Validate form inputs
+        if not form_note_title:
+            error = error | 1
+        if not form_note_text:
+            error = error | 2
+        if not topic_store.topic_exists(topic_map.identifier, form_note_scope):
+            error = error | 4
+
+        if error != 0:
+            flash(
+                'An error occurred when submitting the form. Please review the warnings and fix accordingly.',
+                'warning')
+        else:
+            note_occurrence = Occurrence(instance_of='note', topic_identifier='notes',
+                                         scope=form_note_scope,
+                                         resource_data=form_note_text)
+            title_attribute = Attribute('title', form_note_title, note_occurrence.identifier,
+                                        data_type=DataType.STRING)
+            timestamp = str(datetime.now())
+            modification_attribute = Attribute('modification-timestamp', timestamp, note_occurrence.identifier,
+                                               data_type=DataType.TIMESTAMP)
+
+            # Persist objects to the topic store
+            topic_store.set_occurrence(topic_map.identifier, note_occurrence)
+            topic_store.set_attribute(topic_map.identifier, title_attribute)
+            topic_store.set_attribute(topic_map.identifier, modification_attribute)
+
+            flash('Note successfully added.', 'success')
+            return redirect(
+                url_for('topic.view', map_identifier=topic_map.identifier, topic_identifier=topic.identifier))
+
     return render_template('note/add.html',
-                           topic_map=topic_map)
+                           error=error,
+                           topic_map=topic_map,
+                           topic=topic,
+                           note_title=form_note_title,
+                           note_text=form_note_text,
+                           note_scope=form_note_scope)
