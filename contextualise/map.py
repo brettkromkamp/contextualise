897aee437ffa7f37b2e45c37a5be22951eed23b2
Brett Kromkamp <brett.kromkamp@gmail.com>
2019-06-11 19:49:27 +0200

    Initial (unfinished) implementation of map creation functionality.

--- contextualise/map.py
+++ contextualise/map.py
@@ -1,9 +1,12 @@
-from flask import (Blueprint, session, render_template)
+from flask import (Blueprint, session, request, render_template)
 from flask_security import login_required, current_user
 
 from contextualise.topic_store import get_topic_store
 
 bp = Blueprint('map', __name__)
+
+RESOURCES_DIRECTORY = 'static/resources/'
+EXTENSIONS_WHITELIST = {'png', 'jpg', 'jpeg', 'gif'}
 
 
 @bp.route('/maps/')
@@ -31,3 +34,34 @@ def shared():
     session['current_scope'] = '*'
 
     return render_template('map/shared.html', maps=maps)
+
+
+@bp.route('/maps/create/')
+@login_required
+def create():
+    topic_store = get_topic_store()
+
+    form_name = ''
+    form_description = ''
+    form_shared = False
+
+    error = 0
+
+    if request.method == 'POST':
+        pass
+
+    return render_template('map/create.html',
+                           error=error,
+                           map_name=form_name,
+                           map_description=form_description,
+                           map_shared=form_shared)
+
+
+# ========== HELPER METHODS ==========
+
+def get_file_extension(file_name):
+    return file_name.rsplit('.', 1)[1].lower()
+
+
+def allowed_file(file_name):
+    return get_file_extension(file_name) in EXTENSIONS_WHITELIST
