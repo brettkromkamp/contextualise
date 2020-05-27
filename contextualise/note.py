4db98d0834a91dcb0fb09aeff36c2239645bb53a
Brett Kromkamp <brettkromkamp@gmail.com>
2020-05-27 06:38:43 +0200

    Add initial 'edit note' functionality for unattached notes.

--- contextualise/note.py
+++ contextualise/note.py
@@ -24,17 +24,14 @@ def index(map_identifier):
     topic_store = get_topic_store()
 
     if current_user.is_authenticated:  # User is logged in
-        is_map_owner = topic_store.is_topic_map_owner(
-            map_identifier, current_user.id)
+        is_map_owner = topic_store.is_topic_map_owner(map_identifier, current_user.id)
         if is_map_owner:
-            topic_map = topic_store.get_topic_map(
-                map_identifier, current_user.id)
+            topic_map = topic_store.get_topic_map(map_identifier, current_user.id)
         else:
             topic_map = topic_store.get_topic_map(map_identifier)
         if topic_map is None:
             abort(404)
-        collaboration_mode = topic_store.get_collaboration_mode(
-            map_identifier, current_user.id)
+        collaboration_mode = topic_store.get_collaboration_mode(map_identifier, current_user.id)
         # The map is private and doesn't belong to the user who is trying to
         # access it
         if not topic_map.published and not is_map_owner:
@@ -84,10 +81,7 @@ def add(map_identifier):
     if not topic_map.owner and topic_map.collaboration_mode is not CollaborationMode.EDIT:
         abort(403)
 
-    topic = topic_store.get_topic(
-        map_identifier,
-        "home",
-        resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
+    topic = topic_store.get_topic(map_identifier, "home", resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
     if topic is None:
         abort(404)
 
@@ -134,12 +128,10 @@ def add(map_identifier):
             # Persist objects to the topic store
             topic_store.set_occurrence(topic_map.identifier, note_occurrence)
             topic_store.set_attribute(topic_map.identifier, title_attribute)
-            topic_store.set_attribute(
-                topic_map.identifier, modification_attribute)
+            topic_store.set_attribute(topic_map.identifier, modification_attribute)
 
             flash("Note successfully added.", "success")
-            return redirect(
-                url_for("note.index", map_identifier=topic_map.identifier))
+            return redirect(url_for("note.index", map_identifier=topic_map.identifier))
 
     return render_template(
         "note/add.html",
@@ -152,8 +144,7 @@ def add(map_identifier):
     )
 
 
-@bp.route("/notes/attach/<map_identifier>/<note_identifier>",
-          methods=("GET", "POST"))
+@bp.route("/notes/attach/<map_identifier>/<note_identifier>", methods=("GET", "POST"))
 @login_required
 def attach(map_identifier, note_identifier):
     topic_store = get_topic_store()
@@ -166,10 +157,7 @@ def attach(map_identifier, note_identifier):
     if not topic_map.owner and topic_map.collaboration_mode is not CollaborationMode.EDIT:
         abort(403)
 
-    topic = topic_store.get_topic(
-        map_identifier,
-        "home",
-        resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
+    topic = topic_store.get_topic(map_identifier, "home", resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
     if topic is None:
         abort(404)
 
@@ -190,8 +178,7 @@ def attach(map_identifier, note_identifier):
         form_note_topic_identifier = request.form["note-topic-identifier"].strip()
 
         # Validate form inputs
-        if not topic_store.topic_exists(
-                topic_map.identifier, form_note_topic_identifier):
+        if not topic_store.topic_exists(topic_map.identifier, form_note_topic_identifier):
             error = error | 1
 
         if error != 0:
@@ -200,8 +187,7 @@ def attach(map_identifier, note_identifier):
                 "warning",
             )
         else:
-            topic_store.update_occurrence_topic_identifier(
-                map_identifier, note_identifier, form_note_topic_identifier)
+            topic_store.update_occurrence_topic_identifier(map_identifier, note_identifier, form_note_topic_identifier)
             flash("Note successfully attached.", "success")
             return redirect(
                 url_for(
@@ -223,8 +209,7 @@ def attach(map_identifier, note_identifier):
     )
 
 
-@bp.route("/notes/convert/<map_identifier>/<note_identifier>",
-          methods=("GET", "POST"))
+@bp.route("/notes/convert/<map_identifier>/<note_identifier>", methods=("GET", "POST"))
 @login_required
 def convert(map_identifier, note_identifier):
     topic_store = get_topic_store()
@@ -237,10 +222,7 @@ def convert(map_identifier, note_identifier):
     if not topic_map.owner and topic_map.collaboration_mode is not CollaborationMode.EDIT:
         abort(403)
 
-    topic = topic_store.get_topic(
-        map_identifier,
-        "home",
-        resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
+    topic = topic_store.get_topic(map_identifier, "home", resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
 
     if topic is None:
         abort(404)
@@ -255,8 +237,7 @@ def convert(map_identifier, note_identifier):
 
     form_topic_name = ""
     form_topic_identifier = ""
-    form_topic_text = "## " + note_title + "\n" + \
-        note_occurrence.resource_data.decode()
+    form_topic_text = "## " + note_title + "\n" + note_occurrence.resource_data.decode()
 
     form_topic_instance_of = "topic"
 
@@ -305,12 +286,10 @@ def convert(map_identifier, note_identifier):
             # Persist objects to the topic store
             topic_store.set_topic(topic_map.identifier, new_topic)
             topic_store.set_occurrence(topic_map.identifier, text_occurrence)
-            topic_store.set_attribute(
-                topic_map.identifier, modification_attribute)
+            topic_store.set_attribute(topic_map.identifier, modification_attribute)
 
             # Remove the original note occurrence
-            topic_store.delete_occurrence(
-                topic_map.identifier, note_identifier)
+            topic_store.delete_occurrence(topic_map.identifier, note_identifier)
 
             flash("Note successfully converted.", "success")
             return redirect(
@@ -331,4 +310,106 @@ def convert(map_identifier, note_identifier):
         topic_text=form_topic_text,
         topic_instance_of=form_topic_instance_of,
         note_identifier=note_identifier,
+    )
+
+
+@bp.route("/notes/edit/<map_identifier>/<note_identifier>", methods=("GET", "POST"))
+@login_required
+def edit(map_identifier, note_identifier):
+    topic_store = get_topic_store()
+
+    topic_map = topic_store.get_topic_map(map_identifier, current_user.id)
+    if topic_map is None:
+        abort(404)
+
+    # If the map doesn't belong to the user and they don't have the right
+    # collaboration mode on the map, then abort
+    if not topic_map.owner and (
+            topic_map.collaboration_mode is not CollaborationMode.EDIT
+            and topic_map.collaboration_mode is not CollaborationMode.COMMENT
+    ):
+        abort(403)
+
+    topic = topic_store.get_topic(map_identifier, "home", resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES)
+
+    if topic is None:
+        abort(404)
+
+    note_occurrence = topic_store.get_occurrence(
+        map_identifier,
+        note_identifier,
+        inline_resource_data=RetrievalMode.INLINE_RESOURCE_DATA,
+        resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES,
+    )
+
+    form_note_title = note_occurrence.get_attribute_by_name("title").value
+    form_note_text = note_occurrence.resource_data.decode()
+    form_note_scope = note_occurrence.scope
+
+    error = 0
+
+    if request.method == "POST":
+        form_note_title = request.form["note-title"].strip()
+        form_note_text = request.form["note-text"].strip()
+        form_note_scope = request.form["note-scope"].strip()
+
+        # If no values have been provided set their default values
+        if not form_note_scope:
+            form_note_scope = UNIVERSAL_SCOPE
+
+        # Validate form inputs
+        if not form_note_title:
+            error = error | 1
+        if not form_note_text:
+            error = error | 2
+        if not topic_store.topic_exists(topic_map.identifier, form_note_scope):
+            error = error | 4
+
+        if error != 0:
+            flash(
+                "An error occurred when submitting the form. Please review the warnings and fix accordingly.",
+                "warning",
+            )
+        else:
+            # Update note's title if it has changed
+            if note_occurrence.get_attribute_by_name("title").value != form_note_title:
+                topic_store.update_attribute_value(
+                    topic_map.identifier, note_occurrence.get_attribute_by_name(
+                        "title").identifier, form_note_title,
+                )
+
+            # Update the note's modification (timestamp) attribute
+            timestamp = str(datetime.now())
+            topic_store.update_attribute_value(
+                topic_map.identifier,
+                note_occurrence.get_attribute_by_name(
+                    "modification-timestamp").identifier,
+                timestamp,
+            )
+
+            # Update note (occurrence)
+            topic_store.update_occurrence_data(map_identifier, note_occurrence.identifier, form_note_text)
+
+            # Update note's scope if it has changed
+            if note_occurrence.scope != form_note_scope:
+                topic_store.update_occurrence_scope(map_identifier, note_occurrence.identifier, form_note_scope)
+
+            flash("Note successfully updated.", "success")
+            return redirect(
+                url_for(
+                    "note.index",
+                    map_identifier=topic_map.identifier,
+                    topic_identifier=topic.identifier,
+                )
+            )
+
+    return render_template(
+        "note/edit.html",
+        error=error,
+        topic_map=topic_map,
+        topic=topic,
+        note_identifier=note_occurrence.identifier,
+        note_title=form_note_title,
+        note_text=form_note_text,
+        note_scope=form_note_scope,
     )
