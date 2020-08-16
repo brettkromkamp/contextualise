6f6ef4b1a9ad6aa94a0cfb8bf974202f2fae20e8
Brett Kromkamp <brettkromkamp@gmail.com>
2020-08-16 15:17:56 +0200

    Replace custom (3D) model viewer with google/model-viewer

--- contextualise/three_d.py
+++ contextualise/three_d.py
@@ -282,6 +282,30 @@ def delete(map_identifier, topic_identifier, file_identifier):
     )
 
 
+@bp.route(
+    "/3d/view/<map_identifier>/<topic_identifier>/<file_url>", methods=("GET", "POST"),
+)
+@login_required
+def view(map_identifier, topic_identifier, file_url):
+    topic_store = get_topic_store()
+
+    topic_map = topic_store.get_topic_map(map_identifier, current_user.id)
+    if topic_map is None:
+        abort(404)
+    # If the map doesn't belong to the user and they don't have the right
+    # collaboration mode on the map, then abort
+    if not topic_map.owner and topic_map.collaboration_mode is not CollaborationMode.EDIT:
+        abort(403)
+
+    topic = topic_store.get_topic(
+        map_identifier, topic_identifier, resolve_attributes=RetrievalMode.RESOLVE_ATTRIBUTES,
+    )
+    if topic is None:
+        abort(404)
+
+    return render_template("three_d/view.html", topic_map=topic_map, topic=topic, file_url=file_url)
+
+
 # ========== HELPER METHODS ==========
 
 
