246ec80062d5078e858e95801dd845d727492175
Brett Kromkamp <brettkromkamp@gmail.com>
2024-10-05 13:15:31 +0200

    Config changes :(

--- src/contextualise/config.py
+++ src/contextualise/config.py
@@ -10,11 +10,12 @@ Brett Alistair Kromkamp (brettkromkamp@gmail.com)
 import os
 
 
-DATABASE_FILE = os.getenv("DATABASE_FILE", ":memory:")
-EMAIL_USERNAME = os.getenv("EMAIL_USERNAME", "changeme@changeme.com")
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "changeme")
-EMAIL_SERVER = os.getenv("EMAIL_SERVER", "smtp.changeme.com")
-EMAIL_SENDER = os.getenv("EMAIL_SENDER", "Change Me <changeme@changeme.com>")
-EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
-SECRET_KEY = os.getenv("SECRET_KEY", "changeme-secret-key")
-SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT", "changeme-password-salt")
+class Config:
+    DATABASE_FILE = os.getenv("DATABASE_FILE", "contextualise.sqlite3")
+    EMAIL_USERNAME = os.getenv("EMAIL_USERNAME", "changeme@changeme.com")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "changeme")
+    EMAIL_SERVER = os.getenv("EMAIL_SERVER", "smtp.changeme.com")
+    EMAIL_SENDER = os.getenv("EMAIL_SENDER", "Change Me <changeme@changeme.com>")
+    EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
+    SECRET_KEY = os.getenv("SECRET_KEY", "changeme-secret-key")
+    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT", "changeme-password-salt")
