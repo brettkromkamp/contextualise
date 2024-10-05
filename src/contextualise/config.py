2c1f8cf2f5a0b7a5f857d48ef5819b10bce85a74
Brett Kromkamp <brettkromkamp@gmail.com>
2024-10-05 07:32:16 +0200

    Further Docker configuration pluse minor template fixes

---
+++ src/contextualise/config.py
@@ -0,0 +1,20 @@
+"""
+config.py file. Part of the Contextualise project.
+
+This module defines project-level constants.
+
+October 5, 2024
+Brett Alistair Kromkamp (brettkromkamp@gmail.com)
+"""
+
+import os
+
+class Config:
+    DATABASE_FILE = os.getenv("DATABASE_FILE", ":memory:")  # If not set, use in-memory database
+    EMAIL_USERNAME = os.getenv("EMAIL_USERNAME", "changeme@changeme.com")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD", "changeme")
+    EMAIL_SERVER = os.getenv("EMAIL_SERVER", "smtp.changeme.com")
+    EMAIL_SENDER = os.getenv("EMAIL_SENDER", "Change Me <changeme@changeme.com>")
+    EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
+    SECRET_KEY = os.getenv("SECRET_KEY", "changeme-secret-key")
+    SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT", "changeme-password-salt")
--- src/contextualise/constants.py
+++ src/contextualise/constants.py
@@ -9,8 +9,8 @@ Brett Alistair Kromkamp (brettkromkamp@gmail.com)
 
 import os
 
+BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 UNIVERSAL_SCOPE = "*"
-SETTINGS_FILE_PATH = os.path.join(os.path.dirname(__file__), "../settings.ini")
 RESOURCES_DIRECTORY = "resources"
 IMAGE_EXTENSIONS_WHITELIST = {"png", "jpg", "jpeg", "gif"}
 THREE_D_EXTENSIONS_WHITELIST = {"gltf", "glb"}
